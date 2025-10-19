0f77a61258ff57746b21eb84b4229e93737daaa9
Akshat Kumar <akshatkumar2022@vitbhopal.ac.in>
2025-10-20 02:39:07 +0530

    created functions for user interaction

--- client/context/ChatContext.jsx
+++ client/context/ChatContext.jsx
@@ -1,8 +1,60 @@
-import { createContext } from "react";
+import { createContext, useContext, useState } from "react";
+import { AuthContext } from "./AuthContext";
 
 export const ChatContext = createContext();
 
 export const ChatProvider = ({ children }) => {
+  const [messages, setMessages] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [selectedUser, setSelectedUser] = useState(null);
+  const [unseenMessages, setUnseenMessages] = useState({});
+
+  const { socket, axios } = useContext(AuthContext);
+
+  // Function to get all users for sidebar:
+  const getUsers = async () => {
+    try {
+      const { data } = await axios.get("/api/messages/users");
+      if (data.success) {
+        setUsers(data.users);
+        setUnseenMessages(data.unseenMessages);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  // Function to get messages for selected users:
+  const getMessages = async (userId) => {
+    try {
+      const { data } = await axios.get(`/api/messages/${userId}`);
+      if (data.success) {
+        setMessages(data.messages);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  // Function to send message to selected users:
+  const sendMessage = async (messageData) => {
+    try {
+      const { data } = await axios.post(`/api/messages//${selectedUser._id}`);
+      if (data.success) {
+        setMessages((prevMessages) => [...prevMessages, data.newMessage]);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  // Function to subscribe for selected users:
+  const subscribeToMessages = async () => {
+    //
+  };
+
   const value = {};
   return <ChatContext.Provider value={value}>{children}</ChatContext.Provider>;
 };
