15fe20f99e7f93894f44be5c3b0a53b9cb6b1783
Akshat Kumar <akshatkumar2022@vitbhopal.ac.in>
2025-10-22 00:58:48 +0530

    public file config

--- server/server.js
+++ server/server.js
@@ -48,7 +48,12 @@ app.use("/api/messages", messageRouter);
 // Connect to MongoDB
 await connectDB(); 
 
-const PORT = process.env.PORT || 5001; 
-server.listen(PORT, () => {
-  console.log(`Server is running on PORT: ${PORT}`); 
-})
+if (process.env.NODE_ENV != "production") {
+  const PORT = process.env.PORT || 5001; 
+  server.listen(PORT, () => {
+    console.log(`Server is running on PORT: ${PORT}`); 
+  })
+} 
+
+// Export server for Vercel: 
+export default server; 
